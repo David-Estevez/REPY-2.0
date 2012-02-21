d6c19dc32564f40f3f13f14700b0efcd26460d83
David Estevez Fernandez <david.estevez.fdez@gmail.com>
2012-02-21 11:07:47 +0100

    need some debuggin'

--- HARDWARE/main.cpp
+++ HARDWARE/main.cpp
@@ -35,17 +35,69 @@ struct Ear
 
 };
 
-struct Point _2ndDegree(double a, double b, double c)
+double _2ndDegree(double a, double b, double c, bool negative)
 {
-    struct Point solution;
-    solution.x = (-b + sqrt( pow(b, 2) -4*a*c)) / (2*a);
-    solution.y = (-b - sqrt( pow(b, 2) -4*a*c)) / (2*a);
+    double solution;
+    if (pow(b,2) - 4*a*c < 0)
+        cerr << "Error. No solutions for 2nd order eq. " << endl;
+    if (negative) solution = (-b - sqrt( pow(b, 2) -4*a*c)) / (2*a);
+    else solution = (-b + sqrt( pow(b, 2) -4*a*c)) / (2*a);
     return solution;
 }
+
 struct Point tang_points(double origin_x, double origin_y, double center_x, double center_y, double radius, bool leftmost)
 {
+        //-- Get the line where the tangent points should be
+        double m = (center_x - origin_x) / (origin_y - center_y);
+        double n = -center_x * m + center_y + pow(radius ,2)/(origin_y-center_y);
 
-}
+        //-- There must be two tangential points
+        struct Point tangent[2];
+        struct Point dummy[4];  //-- Dummy variable for storing solutions
+
+        //-- Obtain potential solutions
+        dummy[0].x = _2ndDegree( pow(m, 2)+1, 2*(m*n - m*center_y - center_x), pow(center_x, 2) + pow(n, 2) -2*center_y*n + pow(center_y, 2) - pow( radius, 2) , false);
+        dummy[0].y = _2ndDegree( 1, -2*center_y, pow( (dummy[0].x - center_x), 2) + pow(center_y, 2) - pow(radius, 2), false);
+        dummy[1].x = dummy[0].x;
+        dummy[1].y = _2ndDegree( 1, -2*center_y, pow( (dummy[1].x - center_x), 2) + pow(center_y, 2) - pow(radius, 2), true);
+        dummy[2].x = _2ndDegree( pow(m, 2)+1, 2*(m*n - m*center_y - center_x), pow(center_x, 2) + pow(n, 2) -2*center_y*n + pow(center_y, 2) - pow( radius, 2) , true);
+        dummy[2].y = _2ndDegree( 1, -2*center_y, pow( (dummy[2].x - center_x), 2) + pow(center_y, 2) - pow(radius, 2), false);
+        dummy[3].x = dummy[2].x;
+        dummy[3].y = _2ndDegree( 1, -2*center_y, pow( (dummy[3].x - center_x), 2) + pow(center_y, 2) - pow(radius, 2), true);
+
+        int solutions = 0; //-- Solutions already found
+        int i = 0;
+
+        while (solutions < 2 && i < 4)
+        {
+           double check = pow( (dummy[i].x - center_x), 2) + pow( (dummy[i].y - center_y), 2) - pow(radius, 2);
+
+           if ( check < 0.0001 && check >-0.0001) //-- Check if it is aproximately 0
+           {
+               tangent[solutions].x = dummy[i].x;
+               tangent[solutions].y = dummy[i].y;
+               solutions++;
+           }
+           i++;
+         }
+
+        //-- Choose leftmost or rightmost solution
+         if (leftmost)
+        {
+             if (tangent[0].x < tangent[1].x)
+                 return tangent[0];
+             else
+                 return tangent[1];
+         }
+         else
+         {
+             if (tangent[0].x > tangent[1].x)
+                 return tangent[0];
+             else
+                 return tangent[1];
+         }
+ }
+
 
 void part01(struct Base base)
 {
@@ -97,6 +149,7 @@ void part02(struct Ear ear)
 
     //-- Compute the servo's corona circle
     Component2D corona = Circle::create(ear.corona_r, 100);
+    corona.translate(0, ear.height, 0);
 
     //-- Create the ear shape
     Polygon<Point2D> shape;
@@ -106,8 +159,8 @@ void part02(struct Ear ear)
 
     //-- Compute tangent points
     struct Point tang[2];
-    tang[0] = tang_points(ear.base/2, ear.no_name, 0, ear.height, ear.corona_r, true);
-    tang[1] = tang_points(-ear.base/2, ear.no_name, 0, ear.height, ear.corona_r, false);
+    tang[0] = tang_points(ear.base/2, ear.no_name, 0, ear.height, ear.corona_r, false);
+    tang[1] = tang_points(-ear.base/2, ear.no_name, 0, ear.height, ear.corona_r, true);
     shape.addPoint(Point2D(tang[0].x,tang[0].y));
     shape.addPoint(Point2D(tang[1].x,tang[1].y));
 
@@ -117,8 +170,8 @@ void part02(struct Ear ear)
 
     //-- Extrude the ear shape
     Component ear_shape = PolygonalPrism(shape, ear.thickness);
-
-    Component ear_result = corona + ear_shape;
+    Component ear_extrude = corona.linearExtrudedCopy(ear.thickness,0,100,10,false);
+    Component ear_result = ear_extrude + ear_shape;
 
     writer << ear_result;
 
@@ -128,11 +181,12 @@ void part02(struct Ear ear)
 int main()
 {
     struct Base base = {52, 4, 15, 15, 3/2, 40, 20};
+    struct Ear ear = { 50, 10, 50, 25/2, 3/2, 4};
 
     //part01( base);
 
-    //part02(ear);
-
+    part02(ear);
+    //cout << _2ndDegree( 2 , 4, -5, true) << endl << _2ndDegree(2,4,-5,false);
     //cout << "Done!" << endl;
 
     return 0;
